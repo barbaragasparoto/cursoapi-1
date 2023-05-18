f62c832162092d1fbfb3c7290d961a8c50b087b7
NaraCamargo <naraliggia2@gmail.com>
2023-05-18 08:57:05 -0300

    18/05 continuação do predicate

--- src/main/java/br/com/etec/nara/cursoapi/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/nara/cursoapi/repository/curso/CursoRepositoryImpl.java
@@ -3,18 +3,21 @@ package br.com.etec.nara.cursoapi.repository.curso;
 
 import br.com.etec.nara.cursoapi.model.Curso;
 import br.com.etec.nara.cursoapi.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.util.Predicates;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CursoRepositoryImpl implements CursoRepositoryQuery {
 
@@ -30,14 +33,22 @@ public class CursoRepositoryImpl implements CursoRepositoryQuery {
 
         Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
         criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("nomecurso")));
+
+        TypedQuery<Curso> query = manager.createQuery(criteria);
+
 
         return null;
     }
 
     private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
 
-        List<Predicates> predicates = new ArrayList<>();
+        List<Predicate> predicates = new ArrayList<>();
 
-        if(StringUtils)
+        if(!StringUtils.isEmpty(cursoFilter.getNomecurso())) {
+            predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+                    "%" + cursoFilter.getNomecurso().toLowerCase()));
+        }
+        return predicates.toArray(new Predicate[predicates.size()]);
     }
 }
